5a37ac06b0786f23758157e52f5dbb5cf267156a
kathyrazaf01 <liantsoakathyrazaf@gmail.com>
2024-03-19 10:31:26 +0300

    tag manager

--- index.php
+++ index.php
@@ -15,6 +15,13 @@ else {
 <!DOCTYPE html>
 <html>
 	<head>
+		<!-- Google Tag Manager -->
+			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-N89BJNV6');</script>
+		<!-- End Google Tag Manager -->
 		<title>Welcome to nita's online grocery</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -136,4 +143,10 @@ else {
 			</div>			
 		</div>
 	</body>
+
+	<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N89BJNV6"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+
 </html>
